41eda2b1e5588d7f2e598a81818f205df91e349d
Alex Tacescu <alextac98@gmail.com>
2025-10-02 23:25:30 -0700

    GH-3: Clean up C/C++ code and examples

--- cpp/examples/demo_cpp.cpp
+++ cpp/examples/demo_cpp.cpp
@@ -2,10 +2,9 @@
 #include <iostream>
 
 int main() {
-    using namespace dvpp;
-    Var d(10.0, "m");
-    Var t(2.0, "s");
-    Var v = d / t; // 5 m/s
+    dv::DV d(10.0, "m");
+    dv::DV t(2.0, "s");
+    dv::DV v = d / t; // 5 m/s
     double mph = v.value_in("mi/hr");
     std::cout << "speed = " << mph << " mi/hr\n";
     return 0;
--- cpp/include/dv.hpp
+++ cpp/include/dv.hpp
@@ -3,24 +3,24 @@
 #include <stdexcept>
 #include <string>
 
-namespace dvpp {
+namespace dv {
 
 inline std::string last_error() {
     const char* msg = dv_last_error_message();
     return msg ? std::string(msg) : std::string();
 }
 
-class Var {
+class DV {
 public:
-    Var() : ptr_(nullptr) {}
-    Var(double value, const char* unit) : ptr_(dv_var_new(value, unit)) {
+    DV() : ptr_(nullptr) {}
+    DV(double value, const char* unit) : ptr_(dv_var_new(value, unit)) {
         if (!ptr_) throw std::runtime_error(last_error());
     }
-    Var(const Var&) = delete;
-    Var& operator=(const Var&) = delete;
-    Var(Var&& o) noexcept : ptr_(o.ptr_) { o.ptr_ = nullptr; }
-    Var& operator=(Var&& o) noexcept { if (this != &o) { reset(); ptr_ = o.ptr_; o.ptr_ = nullptr; } return *this; }
-    ~Var() { reset(); }
+    DV(const DV&) = delete;
+    DV& operator=(const DV&) = delete;
+    DV(DV&& o) noexcept : ptr_(o.ptr_) { o.ptr_ = nullptr; }
+    DV& operator=(DV&& o) noexcept { if (this != &o) { reset(); ptr_ = o.ptr_; o.ptr_ = nullptr; } return *this; }
+    ~DV() { reset(); }
 
     void reset() { if (ptr_) { dv_var_free(ptr_); ptr_ = nullptr; } }
     bool valid() const { return ptr_ != nullptr; }
@@ -38,23 +38,23 @@ public:
     }
 
     // Arithmetic: return new objects
-    friend Var operator+(const Var& a, const Var& b) { return from_new(dv_var_add(a.cptr(), b.cptr())); }
-    friend Var operator-(const Var& a, const Var& b) { return from_new(dv_var_sub(a.cptr(), b.cptr())); }
-    friend Var operator*(const Var& a, const Var& b) { return from_new(dv_var_mul(a.cptr(), b.cptr())); }
-    friend Var operator/(const Var& a, const Var& b) { return from_new(dv_var_div(a.cptr(), b.cptr())); }
+    friend DV operator+(const DV& a, const DV& b) { return from_new(dv_var_add(a.cptr(), b.cptr())); }
+    friend DV operator-(const DV& a, const DV& b) { return from_new(dv_var_sub(a.cptr(), b.cptr())); }
+    friend DV operator*(const DV& a, const DV& b) { return from_new(dv_var_mul(a.cptr(), b.cptr())); }
+    friend DV operator/(const DV& a, const DV& b) { return from_new(dv_var_div(a.cptr(), b.cptr())); }
 
-    friend Var operator*(const Var& a, double s) { return from_new(dv_var_mul_scalar(a.cptr(), s)); }
-    friend Var operator*(double s, const Var& a) { return from_new(dv_var_mul_scalar(a.cptr(), s)); }
-    friend Var operator/(const Var& a, double s) { return from_new(dv_var_div_scalar(a.cptr(), s)); }
+    friend DV operator*(const DV& a, double s) { return from_new(dv_var_mul_scalar(a.cptr(), s)); }
+    friend DV operator*(double s, const DV& a) { return from_new(dv_var_mul_scalar(a.cptr(), s)); }
+    friend DV operator/(const DV& a, double s) { return from_new(dv_var_div_scalar(a.cptr(), s)); }
 
-    Var powi(int e) const { return from_new(dv_var_powi(cptr(), e)); }
-    Var powf(double e) const { return from_new(dv_var_powf(cptr(), e)); }
-    Var sqrt() const { return from_new(dv_var_sqrt(cptr())); }
+    DV powi(int e) const { return from_new(dv_var_powi(cptr(), e)); }
+    DV powf(double e) const { return from_new(dv_var_powf(cptr(), e)); }
+    DV sqrt() const { return from_new(dv_var_sqrt(cptr())); }
 
 private:
-    static Var from_new(dv_var* p) {
+    static DV from_new(dv_var* p) {
         if (!p) throw std::runtime_error(last_error());
-        Var v; v.ptr_ = p; return v;
+        DV v; v.ptr_ = p; return v;
     }
 
     dv_var* ptr_;
