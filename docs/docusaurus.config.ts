1f90f13513ba960fded44da5d9ed39d6e181fd19
Alex Tacescu <alextac98@gmail.com>
2025-10-21 00:41:54 -0700

    GH-24: Add system darkness mode as default

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -53,6 +53,11 @@ const config: Config = {
   themeConfig: {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
+    colorMode: {
+      defaultMode: 'light',
+      disableSwitch: false,
+      respectPrefersColorScheme: true,
+    },
     navbar: {
       title: 'DimensionalVariable',
       logo: {
